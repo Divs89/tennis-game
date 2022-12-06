fed9ec8a54e66c9e4389a40b16a5bea54d791804
Sathiya Narayanan <divya-sathiya.narayanan@capgemini.com>
2022-12-06 13:49:47 +0530

    Added test Case player2 scores 3 point

--- src/main/java/com/tennis/tennisGame/Service/TennisService.java
+++ src/main/java/com/tennis/tennisGame/Service/TennisService.java
@@ -1,11 +1,23 @@
 package com.tennis.tennisGame.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class TennisService {
 	int playerOneScore;
 	int playerTwoScore;
+	
+	Map<Integer,String> score = new HashMap<>();
+	
+	TennisService(){
+		score.put(0,"Love");
+		score.put(1,"Fifteen");
+		score.put(2,"Thirty");
+		score.put(3,"Forty");
+	}
 
 	public String getScore(int playerOne, int playerTwo) {
 		this.playerOneScore = playerOne;
@@ -15,8 +27,8 @@ public class TennisService {
 			return "Love-All";
 		}
 		
-		else if(playerOneScore==0 && playerTwoScore ==1) {
-			return "Love-Fifteen";
+		else if(playerOneScore==0 && playerTwoScore >=0) {
+			return "Love-"+score.get(playerTwoScore);
 		}
 		else
 			return "invalid Score";
